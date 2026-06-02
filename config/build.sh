92be7bc52f2671fa97327688534953ffed437d7c
amel <amelbh7533@gmail.com>
2026-06-02 08:22:17 +0000

    fishta 7

--- config/build.sh
+++ config/build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-cd .. # 👈 أضف هذا السطر هنا للعودة إلى المجلد الرئيسي حيث يوجد requirements.txt
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput  #t3 css
 python manage.py migrate
