45aa9a30ba7acff38c2579ea2be03763fcbaff98
amel <amelbh7533@gmail.com>
2026-06-02 17:12:10 +0000

    add requirements.txt

--- config/build.sh
+++ config/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput  #t3 css
-python manage.py migrate
-# إنشاء حساب مسؤول تلقائياً إذا لم يكن موجوداً من قبل
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'AmelSecurePass2026')"
+pip install -r config/requirements.txt
+python config/manage.py collectstatic --noinput
+python config/manage.py migrate
