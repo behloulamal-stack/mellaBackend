5417f622cfd21abf1a654f0d3fe499fc5adabca6
Amel_Bh <behloul.amal@cuniv-naama.dz>
2026-06-02 08:16:12 +0000

    t

--- config/build.sh
+++ config/build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+cd .. # 👈 أضف هذا السطر هنا للعودة إلى المجلد الرئيسي حيث يوجد requirements.txt
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
