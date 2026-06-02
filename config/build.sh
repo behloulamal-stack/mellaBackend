9d7627d37ba8f4771e1e29c0da808bd0c02d6c25
Amel_Bh <behloul.amal@cuniv-naama.dz>
2026-06-02 14:53:53 +0000

    admin
    Automatically create superuser if it doesn't exist.

--- config/build.sh
+++ config/build.sh
@@ -3,3 +3,5 @@ cd config
 pip install -r requirements.txt
 python manage.py collectstatic --noinput  #t3 css
 python manage.py migrate
+# إنشاء حساب مسؤول تلقائياً إذا لم يكن موجوداً من قبل
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'AmelSecurePass2026')"
